513197fabdaabba2ca3ac1cc551be2e8f7eac8a7
Mubasher <mubasherali0331@gmail.com>
2023-06-12 15:10:21 +0500

    Added network list

--- src/components/LayoutSimple.jsx
+++ src/components/LayoutSimple.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { toast } from "react-toastify";
 import { setDarkModeVariables, setLightModeVariables } from "../utils/utils";
@@ -8,14 +8,40 @@ import logoLight from "../assets/logo/dexordi-light.png";
 import logoDark from "../assets/logo/dexordi-dark.png";
 import lightIcon from "../assets/icons/light-icon.png";
 import darkIcon from "../assets/icons/dark-icon.png";
+import CancelIcon from "../assets/icons/CancelIcon";
 import WalletIcon from "../assets/icons/WalletIcon";
 import twitterIcon from "../assets/icons/twitter.svg";
 import mediumIcon from "../assets/icons/medium.svg";
 import telegramIcon from "../assets/icons/telegram.svg";
+import okxWalletIcon from "../assets/icons/okxWalletIcon.png";
+import metamaskWalletIcon from "../assets/icons/metamaskWalletIcon.png";
+import unisatWalletIcon from "../assets/icons/unisatWalletIcon.png";
+
 import { SuccessMessage } from "./Notifications";
+
+const walletOptions = [
+    {
+        icon: okxWalletIcon,
+        value: 1,
+        label: "OKX Wallet",
+        label2: "Support Bitcoin Wallet",
+    },
+    {
+        icon: unisatWalletIcon,
+        value: 2,
+        label: "Unisat Wallet",
+        label2: "Support Bitcoin Wallet",
+    },
+    {
+        icon: metamaskWalletIcon,
+        value: 3,
+        label: "MetaMask Wallet",
+    },
+];
 
 function LayoutSimple() {
     const { authState, updateTheme } = useAuthState();
+    const [walletList, setWalletList] = useState(false);
     const toastRef = useRef();
 
     const handleToastClose = () => {
@@ -29,6 +55,10 @@ function LayoutSimple() {
                 closeToast={handleToastClose}
             />
         ));
+
+    const toggleWalletList = () => {
+        setWalletList((prevState) => !prevState);
+    };
 
     useEffect(() => {
         if (authState.preferDark) {
@@ -56,7 +86,8 @@ function LayoutSimple() {
 
                     <button
                         className="d-btn d-btn-primary flex items-center gap-6"
-                        onClick={notifySuccess}
+                        // onClick={notifySuccess}
+                        onClick={toggleWalletList}
                     >
                         <WalletIcon viewBox="0 0 22 22" classes="icon-s" />
                         Connect Wallet
@@ -65,6 +96,39 @@ function LayoutSimple() {
             </header>
 
             <Outlet />
+
+            {walletList && (
+                <section className="walletList__container backdrop__container">
+                    <section className="wallet__content">
+                        <header>
+                            <h3>Connect your wallet</h3>{" "}
+                            <button onClick={toggleWalletList}>
+                                <CancelIcon />
+                            </button>
+                        </header>
+
+                        <ul>
+                            {walletOptions.map((item) => {
+                                return (
+                                    <li
+                                        className={`${
+                                            item.value === authState.wallet ? "active" : ""
+                                        }`}
+                                        key={item.label}
+                                    >
+                                        <div>
+                                            <h3>{item.label}</h3>
+                                            {item.label2 && <p>{item.label2}</p>}
+                                        </div>
+
+                                        <img src={item.icon} alt={item.label} />
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </section>
+                </section>
+            )}
 
             <footer className="lg:px-40 px-20 py-5 flex justify-between pb-16 simple-footer">
                 <div className="left flex gap-16 items-center">
